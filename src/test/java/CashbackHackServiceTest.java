af61fd00b785cb2aa2bff39b64de228a96338b94
Ksenya Kos <kskosyrevavv@gmail.com>
2026-07-30 19:52:00 +0300

    branch testng and tests

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import ru.netology.service.CashbackHackService;
+
+
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateIfAmountLessThen1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 200;
+        int actual = service.remain(800);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountMoreThen1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 500;
+        int actual = service.remain(1500);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(1000);
+
+        assertEquals(expected, actual);
+    }
+}
